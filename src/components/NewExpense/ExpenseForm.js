2e7eb85275e2f465adc5e42a46d60ab60f9d20c7
Chuck Smith <chuck.smith@opscompass.com>
2022-06-01 08:03:33 -0400

    Toggle new expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -21,7 +21,6 @@ const ExpenseForm = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log('submit');
 
     const expenseData = {
       title: enteredTitle,
@@ -55,6 +54,7 @@ const ExpenseForm = (props) => {
           </div>
         </div>
         <div className="new-expense__actions">
+          <button type="button" onClick={props.onCancel}>Cancel</button>
           <button type="submit">Add Expense</button>
         </div>
       </form>
