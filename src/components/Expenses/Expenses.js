5d0351b2c5420226d1d30724a9d0a86277da0b4a
Chuck Smith <chuck.smith@opscompass.com>
2022-06-01 07:04:26 -0400

    Convert static expenses to mapped

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,28 +13,16 @@ const Expenses = (props) => {
   };
 
   return (
+
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onSelectedYear={filterChangeHandler} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {props.items.map(({ title, amount, date, id }) => (
+        <ExpenseItem
+          title={title}
+          amount={amount}
+          date={date}
+          key={id}/>
+      ))}
     </Card>
   );
 };
